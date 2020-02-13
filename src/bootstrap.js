30973ee597f9b5a345395d11d49229b465e06464
Phil Hawker <philhawker1211@gmail.com>
2020-02-12 17:36:00 -0700

    initial commit

---
+++ src/bootstrap.js
@@ -0,0 +1,5 @@
+import classChallenge, {greeting, sum} from './classChallenge'
+
+console.log(classChallenge())
+console.log(sum(5, 5))
+console.log(greeting)
---
+++ src/classChallenge.js
@@ -0,0 +1,9 @@
+export default function () {
+    return '<div>Class Challenge</div>'
+}
+
+export const greeting = 'Welcome'
+
+export function sum(num1, num2) {
+    return num1 + num2
+}
